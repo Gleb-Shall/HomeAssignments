7235bfcf519377f1305776a02994d1fd4dafab18
bymyshegibar <st128274@student.spbu.ru>
2024-12-20 16:24:44 +0300

    there is still a bug in the code, I'm fixing it

--- lab1/TransformerTest.cpp
+++
@@ -1,7 +0,0 @@
-/*
-   Gleb Shikunov
-   st128274@student.spbu.ru
-   Lab1
-*/
-
-
--- lab1/main.cpp
+++ lab1/main.cpp
@@ -5,4 +5,34 @@
 */
 
 
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include "WorkWithBMP.h"
 
+
+void test()
+{
+
+    std::string path = "river_and_sky.bmp";
+    Image image = Image(3440, 5215, path);
+    std::cout << 3440*5215 << " byte will be ocupied by img";
+    std::vector<unsigned char> vector = image.readToVector(path, 3440*5215);
+    
+   //  for(int i = 0; i<= 10; i++)
+   //  {
+   //      std::cout<<vector[i]<<std::endl;
+   //  }
+    
+   //  image.writeToFile("new.bmp",vector);
+   //  image.writeToRaw("new.raw", vector);
+    image.writeToRaw("cwrotated.raw",image.clockwiseRotate(vector));
+    image.writeToRaw("ccwrotated.raw", image.counterClockwiseRotate(vector));
+    image.saveAsGaussianImage(image.counterClockwiseRotate(vector),5);
+}
+
+int main()
+{
+    test();
+}
